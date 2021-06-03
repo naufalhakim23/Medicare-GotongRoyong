0e729201273c0d69a40c80519c6a4974603b77da
Richardaddyanto <addyantorichard@gmail.com>
2021-06-03 23:59:01 +0800

    Third Version of Android

--- Android/app/src/main/java/com/example/medicare/data/network/ApiLogin.kt
+++ Android/app/src/main/java/com/example/medicare/data/network/ApiLogin.kt
@@ -1,13 +1,11 @@
 package com.example.medicare.data.network
 
-import com.example.medicare.data.network.DiabetesData
-import com.example.medicare.data.network.DiabetesPrediction
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface ApiLogin {
-    @POST("/login") // TODO: 2. Nanti diganti sesuai dengan endpoint, tapi tanpa BaseUrl
+    @POST("login") // TODO: 2. Nanti diganti sesuai dengan endpoint, tapi tanpa BaseUrl
     fun postLoginData(
             @Body loginData: LoginData
     ) : Call<LoginResult>
--- Android/app/src/main/java/com/example/medicare/data/network/ApiService.kt
+++ Android/app/src/main/java/com/example/medicare/data/network/ApiService.kt
@@ -7,7 +7,7 @@ import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface ApiService {
-    @POST("/updateData") // TODO: 2. Nanti diganti sesuai dengan endpoint, tapi tanpa BaseUrl
+    @POST("updateData") // TODO: 2. Nanti diganti sesuai dengan endpoint, tapi tanpa BaseUrl
     fun postDiabetesData(
         @Body diabetesData: DiabetesData
     ) : Call<DiabetesPrediction>
